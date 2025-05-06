9ae3c53eb9daae42a1b4ff7a62adb0c43197a2db
sergeykochiev <sergeyivanovo547@gmail.com>
2025-05-06 21:08:51 +0300

    fixed double usage of address in number field

--- templates/bill_templ.go
+++ templates/bill_templ.go
@@ -83,9 +83,9 @@ func BillTemplate(ci types.CompanyInfo, bil types.BillItemList, client_company_n
 			return templ_7745c5c3_Err
 		}
 		var templ_7745c5c3_Var5 string
-		templ_7745c5c3_Var5, templ_7745c5c3_Err = templ.JoinStringErrs(ci.Address)
+		templ_7745c5c3_Var5, templ_7745c5c3_Err = templ.JoinStringErrs(ci.Number)
 		if templ_7745c5c3_Err != nil {
-			return templ.Error{Err: templ_7745c5c3_Err, FileName: `templates/bill.templ`, Line: 20, Col: 23}
+			return templ.Error{Err: templ_7745c5c3_Err, FileName: `templates/bill.templ`, Line: 20, Col: 22}
 		}
 		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ.EscapeString(templ_7745c5c3_Var5))
 		if templ_7745c5c3_Err != nil {
